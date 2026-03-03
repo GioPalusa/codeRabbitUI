563dbaffe0b140f47e984c850cbb60a2e782bc21
Giovanni Palusa <giovanni.palusa@marginalen.se>
2026-03-03 13:23:00 +0100

    Initial Commit

---
+++ CodeRabbit/CodeRabbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeRabbitApp.swift
+//  CodeRabbit
+//
+//  Created by Giovanni Palusa on 2026-03-03.
+//
+
+import SwiftUI
+
+@main
+struct CodeRabbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
